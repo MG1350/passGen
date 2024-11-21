e6ec051c13dca196823a772b80d521a9dc7b92fa
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-11-21 14:59:06 -0500

    .

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,6 +1,11 @@
-public PasswordGenerator(){
+public class PasswordGenerator(){
+    public static void main(String[] args)
+    {
+        System.out.println("Hi");
+        System.out.println("LabOneA");
+    }
 	private int len;
-	private string chars;
+	private String chars;
 	public static int count = 0;
     public PasswordGenerator(int n, String s){
     	len = n;
@@ -8,7 +13,7 @@ public PasswordGenerator(){
     }
 	public PasswordGenerator(int n){
     	len = n;
-        chars = "A"
+        chars = "A";
     }
 	public int pwCount(){
 		return count;
@@ -17,10 +22,11 @@ public PasswordGenerator(){
 		String temp = chars + ".";
 		int iter = len;
         while(len != 0){
-        	temp += (int) (Math.random*9) + 1;
+        	temp += (int) (Math.random()*9) + 1;
 			len--;
         }
       	count++;
         return temp;
 	}
+
 }
